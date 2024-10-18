e8e2f7cf8015c87e62fe2efc1e5ec9f5064d2be9
Pablo <pablolupionmarquez@gmail.com>
2024-10-18 10:20:59 +0200

    commit

--- Triangulo.php
+++ Triangulo.php
@@ -15,7 +15,6 @@ class ProductoFisico extends FiguraGeometrica{
     $this->altura = $altura;
     $this->lado1 = $lado1;
     $this->lado2 = $lado2;
-    $this->lado3 = $lado3;
     }
 
     public function getBase(){
