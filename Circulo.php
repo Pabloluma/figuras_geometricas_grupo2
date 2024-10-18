4bff8f7208eeab7a1ae4bf79b3290b2c80084d11
Antonio-Olivares4 <olivares.lopez.antonio-vladimir@iescamas.es>
2024-10-18 09:17:35 +0200

    Implementada la clase círculo con sus atributos, constructor y métodos

---
+++ Circulo.php
@@ -0,0 +1,28 @@
+<?
+class Circulo{
+    private $centro;
+    private $radio;
+
+    public function __construct($centro, $radio){
+        $this->centro=$centro;
+        $this->radio=$radio;
+
+    }
+
+    public function area(){
+        return M_PI * pow($this->radio, 2);
+    }
+    public function perimetro() {
+        return 2 * M_PI * $this->radio;
+    }
+}
+
+
+
+
+
+
+
+
+
+?>
