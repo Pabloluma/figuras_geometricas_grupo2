b477da75b9c4f6df9739b371b3a3784183873d21
Antonio-Olivares4 <olivares.lopez.antonio-vladimir@iescamas.es>
2024-10-18 09:57:49 +0200

    He puesto el atributo  como de tipo Punto

--- Circulo.php
+++ Circulo.php
@@ -1,14 +1,15 @@
 <?
+
 include_once 'FiguraGeometrica.php';
 
 class Circulo extends FiguraGeometrica{
-    private $centro;
-    private $radio;
+    private Punto $centro;
+    private float $radio;
 
-    public function __construct($centro, $radio){
+    public function __construct(Punto $centro, float $radio){
         $this->centro=$centro;
         $this->radio=$radio;
-
+        
     }
     public function getRadio(){
         return $this->radio;
@@ -20,6 +21,7 @@ class Circulo extends FiguraGeometrica{
     public function perimetro() {
         return 2 * M_PI * $this->radio;
     }
+
 }
 
 
