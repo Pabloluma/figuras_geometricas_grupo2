ee6e48fdd350f238638a15b5d4c2604cea495df1
Antonio-Olivares4 <olivares.lopez.antonio-vladimir@iescamas.es>
2024-10-24 09:21:28 +0200

    Me traigo la clase punto copiándola.

--- CirculoTest.php
+++ CirculoTest.php
@@ -1,7 +1,4 @@
 <?php
 require_once __DIR__ . 'FiguraGeometrica.php';
 require_once __DIR__ . 'Circulo.php';
-
-
-
 ?>
---
+++ Punto.php
@@ -0,0 +1,26 @@
+<?php
+class punto{
+    private float $x;
+    private float $y;
+
+    //Constructor
+    public function __construct($x, $y){
+        $this -> $x=$x;
+        $this -> $y=$y;
+    }
+
+    //Getter
+    public function getX(){
+        return $this -> x;
+    }
+
+    public function getY(){
+        return $this -> y;
+    }
+
+    //Funcion que calcula la distacia que existe entre el punto de origen (0,0) y nuestro punto (x,y)
+    public function distancia(){
+        return sqrt(pow($this->x, 2) + pow($this->y, 2));
+    }
+}
+?>
