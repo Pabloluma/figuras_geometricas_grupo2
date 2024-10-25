839fc0f94c1e4fcdacd69304952e338a04f2ea8d
Pablo <pablolupionmarquez@gmail.com>
2024-10-25 08:33:03 +0200

    Actualización de fichero prueba_triangulo.php

--- prueba_triangulo.php
+++ prueba_triangulo.php
@@ -12,21 +12,21 @@ function assertEquals($expected, $actual, $testName) {
     }
 }
 
-// Pruebas unitarias simples para la clase Circulo
+// Pruebas unitarias simples para la clase Triangulo
 
-// Test 1: Prueba el cálculo del área de un círculo
+// Test 1: Prueba el cálculo del área de un Triangulo
 $triangulo = new Triangulo("Triangulo de prueba", 5);
 $expectedArea = 5 * 5 / 2;
 $actualArea = $triangulo->area();
 assertEquals($expectedArea, $actualArea, "Prueba del cálculo del área");
 
-// Test 2: Prueba el cálculo del perímetro de un círculo
+// Test 2: Prueba el cálculo del perímetro de un Triangulo
 $expectedPerimetro = 6 + 4 + 3;
 $actualPerimetro = $triangulo->perimetro();
 assertEquals($expectedPerimetro, $actualPerimetro, "Prueba del cálculo del perímetro");
 
-// Test 3: Prueba si se retorna correctamente el nombre del círculo
-$expectedNombre = "Círculo de prueba";
+// Test 3: Prueba si se retorna correctamente el nombre del Triangulo
+$expectedNombre = "Triangulo de prueba";
 $actualNombre = $triangulo->getNombre();
 assertEquals($expectedNombre, $actualNombre, "Prueba de obtención del nombre");
 //
