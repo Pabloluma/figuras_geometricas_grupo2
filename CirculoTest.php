e3142a4755715f266ebdba84a121b73dc59bc9f7
IvánPaz4 <109217100+Ivanpaz4@users.noreply.github.com>
2024-10-30 16:37:09 +0100

    testeo cuadrado realizado

--- CirculoTest.php
+++ CirculoTest.php
@@ -1,7 +1,7 @@
-g<?php
+<?php
 // Incluye los archivos necesarios desde la carpeta src
 require_once __DIR__ . '/../src/FiguraGeometrica.php';
-require_once __DIR__ . '/../src/Circulo.php';
+require_once __DIR__ . '/../src/cuadrado.php';
 
 // Función para ejecutar una aserción manual de igualdad
 function assertEquals($expected, $actual, $testName) {
@@ -15,19 +15,19 @@ function assertEquals($expected, $actual, $testName) {
 // Pruebas unitarias simples para la clase Circulo
 
 // Test 1: Prueba el cálculo del área de un círculo
-$circulo = new Circulo("Círculo de prueba", 5);
-$expectedArea = pi() * pow(5, 2);
-$actualArea = $circulo->area();
+$cuadrado = new Cuadrado("Círculo de prueba", 5);
+$expectedArea = $lado * $lado;
+$actualArea = $cuadrado->area();
 assertEquals($expectedArea, $actualArea, "Prueba del cálculo del área");
 
 // Test 2: Prueba el cálculo del perímetro de un círculo
-$expectedPerimetro = 2 * pi() * 5;
-$actualPerimetro = $circulo->perimetro();
+$expectedPerimetro = $lado*4;
+$actualPerimetro = $cuadrado->perimetro();
 assertEquals($expectedPerimetro, $actualPerimetro, "Prueba del cálculo del perímetro");
 
 // Test 3: Prueba si se retorna correctamente el nombre del círculo
-$expectedNombre = "Círculo de prueba";
-$actualNombre = $circulo->getNombre();
+$expectedNombre = "Cudrado de Prueba";
+$actualNombre = $cuadrado->getNombre();
 assertEquals($expectedNombre, $actualNombre, "Prueba de obtención del nombre");
 //
 ?>
